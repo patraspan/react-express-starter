90697fff312d89ea0a7a2a77c847d3853beed320
Patryk <patrick.kurzeja@gmail.com>
2018-07-23 20:26:03 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+
+app.get('/api/customers', (req, res) => {
+  const customers = [{
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      id: 2,
+      firstName: 'Steve',
+      lastName: 'Smith'
+    },
+    {
+      id: 3,
+      firstName: 'mary',
+      lastName: 'Swanson'
+    },
+  ];
+
+  res.json(customers)
+});
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
